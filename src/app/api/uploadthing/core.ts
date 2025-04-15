ae36651b9b3173a687cb6d68fe03353b61ca4b89
CharlieAl <charlie.lerma.ramirez@gmail.com>
2025-04-15 08:52:59 -0700

    feat(users): add banner upload functionality
    Introduce a new modal component for uploading user banners and integrate it with the existing user banner component. Add a new endpoint in the uploadthing core to handle banner uploads, including validation and cleanup of previous banners. This allows users to upload and update their profile banners seamlessly.

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -9,6 +9,59 @@ import { users, videos } from '~/db/schema'
 const f = createUploadthing()
 
 export const ourFileRouter = {
+  bannerUploader: f({
+    image: {
+      maxFileSize: '4MB',
+      maxFileCount: 1
+    }
+  })
+    .middleware(async () => {
+      // This code runs on your server before upload
+      const { userId: clerkUserId } = await auth()
+
+      // If you throw, the user will not be able to upload
+      if (!clerkUserId) throw new UploadThingError('Unauthorized')
+
+      const [existingUser] = await db
+        .select()
+        .from(users)
+        .where(eq(users.clerkId, clerkUserId))
+      if (!existingUser) throw new UploadThingError('Unauthorized')
+
+      if (!existingUser) throw new UploadThingError('Not found')
+      if (existingUser.bannerKey) {
+        const utapi = new UTApi()
+
+        await utapi.deleteFiles(existingUser.bannerKey)
+        await db
+          .update(users)
+          .set({
+            bannerKey: null,
+            bannerUrl: null
+          })
+          .where(and(eq(users.id, existingUser.id)))
+      }
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return {
+        userId: existingUser.id
+      }
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      await db
+        .update(users)
+        .set({
+          bannerUrl: file.ufsUrl,
+          bannerKey: file.key
+        })
+        .where(eq(users.id, metadata.userId))
+
+      console.log('Upload complete for userId:', metadata.userId)
+
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return {
+        uploadedBy: metadata.userId
+      }
+    }),
   thumbnailUploader: f({
     image: {
       maxFileSize: '4MB',
@@ -34,7 +87,9 @@ export const ourFileRouter = {
       if (!user) throw new UploadThingError('Unauthorized')
 
       const [existingVideo] = await db
-        .select({ thumbnailKey: videos.thumbnailKey })
+        .select({
+          thumbnailKey: videos.thumbnailKey
+        })
         .from(videos)
         .where(and(eq(videos.userId, user.id), eq(videos.id, input.videoId)))
       if (!existingVideo) throw new UploadThingError('Not found')
@@ -44,23 +99,34 @@ export const ourFileRouter = {
         await utapi.deleteFiles(existingVideo.thumbnailKey)
         await db
           .update(videos)
-          .set({ thumbnailKey: null, thumbnailUrl: null })
+          .set({
+            thumbnailKey: null,
+            thumbnailUrl: null
+          })
           .where(and(eq(videos.id, input.videoId), eq(videos.userId, user.id)))
       }
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { user, ...input }
+      return {
+        user,
+        ...input
+      }
     })
     .onUploadComplete(async ({ metadata, file }) => {
       const userId = metadata.user.id
       await db
         .update(videos)
-        .set({ thumbnailUrl: file.ufsUrl, thumbnailKey: file.key })
+        .set({
+          thumbnailUrl: file.ufsUrl,
+          thumbnailKey: file.key
+        })
         .where(and(eq(videos.id, metadata.videoId), eq(videos.userId, userId)))
 
       console.log('Upload complete for userId:', userId)
 
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: userId }
+      return {
+        uploadedBy: userId
+      }
     })
 } satisfies FileRouter
 
--- src/modules/users/ui/components/user-banner.tsx
+++ src/modules/users/ui/components/user-banner.tsx
@@ -4,15 +4,24 @@ import { useAuth } from '@clerk/nextjs'
 import { Button } from '~/components/ui/button'
 import { Edit2Icon } from 'lucide-react'
 import { Skeleton } from '~/components/ui/skeleton'
+import { BannerUploadModal } from './banner-upload-modal'
+import { useState } from 'react'
 
 interface UserBannerProps {
   user: UserGetOneOutputs
 }
 
 export const UserBanner = ({ user }: UserBannerProps) => {
+  // este es id de clerk
   const { userId } = useAuth()
+  const [open, setOpen] = useState(false)
   return (
     <div className='relative group'>
+      <BannerUploadModal
+        open={open}
+        onOpenChange={() => setOpen(!open)}
+        userId={user.id}
+      />
       <div
         className={cn(
           'w-full max-h-[200px] h-[15vh] md:h-[25vh] bg-gradient-to-r from-gray-100 to-gray-100 rounded-xl ',
@@ -27,6 +36,7 @@ export const UserBanner = ({ user }: UserBannerProps) => {
             type='button'
             size='icon'
             className='absolute top-4 right-4 bg-black/50 hover:bg-black/50 opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-full'
+            onClick={() => setOpen(!open)}
           >
             <Edit2Icon className='size-4 text-white' />
           </Button>
@@ -37,5 +47,7 @@ export const UserBanner = ({ user }: UserBannerProps) => {
 }
 
 export const UserBannerSkeleton = () => {
-  return <Skeleton className='w-full max-h-[200px] h-[15vh] md:h-[25vh] rounded-xl' />
+  return (
+    <Skeleton className='w-full max-h-[200px] h-[15vh] md:h-[25vh] rounded-xl' />
+  )
 }
